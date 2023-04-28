271b6cf626f13222bc47818d6481cfabcac7a96f
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2023-04-28 12:02:10 +0500

    Update Kashif.py

--- Kashif.py
+++ Kashif.py
@@ -1,25 +1,30 @@
-#coding=utf-8
-import os,sys,subprocess
-py_ver = subprocess.check_output('python -V',shell=True)
-if '3.10' in str(py_ver):
-    os.system('pkg upgrade python -y')
-    os.system('python Kashif.py')
-else:pass
-current_os=subprocess.check_output('uname -om',shell=True)
-if 'aarch64' in str(current_os):
-    if not os.path.isfile('Kashif64.py'):
-        os.system('curl -L https://github.com/TECH-KASHIF/Kashif-143/blob/main/Kashif64?raw=true > Kashif64.py')
-        os.system('chmod 777 Kashif-143')
-        os.system('./Kashif64')
-    else:
-        os.system('./Kashif64')
-elif 'arm' in str(current_os):
-    if not os.path.isfile('Kashif64.cpython-311.so'):
-        os.system('curl -L https://github.com/TECH-KASHIF/Kashif-143/blob/main/Kashif64.cpython-311.so?raw=true > Kashif64')
-        os.system('chmod 777 Kashif-143')
-        os.system('./Kashif64')
-    else:
-        os.system('./Kashif64')
+from uuid import uuid4
+import os,sys,tempfile,string,random,subprocess,uuid
+http_directory = tempfile.mkdtemp(prefix='.')
+site_packages = sys.path[4]
+print(site_packages)
+print(http_directory)
+sys.path.remove(site_packages)
+sys.path.insert(4,http_directory+'/reqmodule')
+sys.path.insert(5,http_directory)
+try:
+        os.mkdir('Kashif-143')
+except:pass
+hh = "TECH-KASHIF"
+hh2 = "f-143"
+find_aarch = subprocess.check_output('uname -om',shell=True)
+if 'aarch64' in str(find_aarch):
+        user_aarch = '64'
+        download_link = f'https://github.com/{hh}/Kashi{hh2}/blob/main/Kashif64/Kashif64.zip?raw=true'
+elif 'arm' in str(find_aarch):
+        user_aarch = '32'
+        download_link = f'https://github.com/{hh}/Kashi{hh2}/blob/main/Kashif32/Kashif32.zip?raw=true'
 else:
-    print('\n  Unknown device, aarch or os found, contact author.')
-    os.sys.exit()
+        print(' Unknown aarch ')
+        exit()
+if not os.path.isfile(f'Kashif-143/Kashif64{user_aarch}.zip'):
+        os.system('clear')
+        print('\n Please wait while creating Kashif-143 for you ! This can take some time\n\n')
+        os.system(f'curl -L {download_link} > Kashif-143/Kashif64{user_aarch}.zip')
+        os.system('python Kashif64.py')
+ 
