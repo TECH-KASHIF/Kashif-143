0eb54e7d09bbb4430360b2aabc69401c9507faab
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2023-04-28 10:26:06 +0500

    Update Kashif.py

--- Kashif.py
+++ Kashif.py
@@ -1,1 +1,8 @@
-import Kashif64
+import os,platform
+os.system('git pull')
+ 
+kashif=platform.architecture()[0]
+if kashif=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif kashif=="64bit":
+    __import__("Kashif64")
