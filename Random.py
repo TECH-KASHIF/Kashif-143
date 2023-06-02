d992fe1ba91b4360c7b67f59d9ad8187e3210bc6
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2023-06-02 23:01:13 +0500

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,31 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from Number import main
+
+    main()
+
+elif bit == '32bit':
+
+    from Number import main
+
+    main()
+
+else:
+
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
+
+    os.system('exit')
